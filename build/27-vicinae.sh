1feaa4040c40e7dbea6360c02717c3b7f43995a3
iker <ikerlarrea@proton.me>
2026-04-11 17:11:47 +0200

    fix: add cmark-gfm dependency for vicinae

--- build/27-vicinae.sh
+++ build/27-vicinae.sh
@@ -7,6 +7,11 @@ echo "Installing Vicinae..."
 # shellcheck source=/dev/null
 source /ctx/build/copr-helpers.sh
 
+# cmark-gfm dependency from quadratech188/cmark-gfm
+copr_install_isolated "quadratech188/cmark-gfm" \
+    cmark-gfm
+
+# vicinae from quadratech188
 copr_install_isolated "quadratech188/vicinae" \
     vicinae
 
